04e03924aadb744ccd68db6daab0375657173add
Michael Scherbela <michael.scherbela@gmail.com>
2020-10-17 22:38:38 +0200

    Initial commit

---
+++ Dockerfile
@@ -0,0 +1,18 @@
+# To build the container, cd into the root directory of the repository (where this Dockerfile is) and type:
+# sudo docker build -t dockermonitor .
+# To run the container, use the following command:
+# sudo docker run --rm -d -v /home/ubuntu/data/dockermonitor:/data dockermonitor:latest
+
+# set base image (host OS)
+FROM python:3.8
+WORKDIR /home
+RUN apt-get update
+
+# set-up the working dir (incl. the code) and install requirements
+COPY requirements.txt .
+RUN pip install -r requirements.txt
+COPY main.py .
+
+ENV FLASK_APP=main.py
+# Run using flask webserver. To be replaced by gunicorn
+CMD ["python", "-m", "flask run"]
---
+++ main.py
@@ -0,0 +1,48 @@
+import flask
+import os
+app = flask.Flask(__name__)
+DATA_DIR = 'data'
+
+class ContainerStatus:
+    def __init__(self, name, image, description=""):
+        self.name = name
+        self.image = image
+        self.description = description
+        self.status_text = ""
+        self.up = False
+
+    def update_status(self, container_data):
+        for c in container_data:
+            if (c['image'] == self.image) and ("up" in c['status'].lower()):
+                self.status_text = c['status']
+                self.up = True
+                break
+        else:
+            self.status_text = ""
+            self.up = False
+
+@app.route('/')
+def hello_world():
+    containers = [ContainerStatus('Paragliding Weather Scraping', 'paragliding:latest', 'Simple webscraper that logs hourly weather condition and forecasts + the status of flying/schooling conditions at Hohe Wand.'),
+                  ContainerStatus('Main webserver', 'nginx_main:latest', 'Main webserver that acts as entry point before dispatching to other sub-servers'),
+                  ContainerStatus('Docker monitor', 'dockermonitor:latest', 'Flask webserver, providing this webpage')]
+    data = getContainerData(DATA_DIR)
+    for c in containers:
+        c.update_status(data)
+    return flask.render_template('dockerstatus.html', containers=containers)
+
+def getContainerData(data_dir):
+    data_rows = []
+    with open(os.path.join(data_dir, 'ps.txt')) as f:
+        for i,l in enumerate(f):
+            if i == 0:
+                continue
+            data = l.split('  ')
+            data = [d.strip() for d in data if len(d)>0]
+            data_rows.append(dict(id=data[0], image=data[1], status=data[4]))
+    return data_rows
+
+if __name__ == '__main__':
+    # data = getContainerData('data')
+    app.run(port=8010)
+
