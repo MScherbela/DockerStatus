eacb57fa1a7ac3a8f7812285f05599c06da5fe74
Michael Scherbela (contabo VPS) <michael.scherbela@gmail.com>
2020-10-20 20:51:16 +0200

    Fully working over SSL (no changes were necessary in this container; still communicating on port 80 to proxy)

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import flask
 import os
+from datetime import datetime
+
 app = flask.Flask(__name__)
 DATA_DIR = '/data'
 
@@ -26,20 +28,25 @@ def hello_world():
     containers = [ContainerStatus('Paragliding Weather Scraping', 'paragliding', 'Simple webscraper that logs hourly weather condition and forecasts + the status of flying/schooling conditions at Hohe Wand.'),
                   ContainerStatus('Main webserver', 'proxy', 'Main webserver that acts as entry point before dispatching to other sub-servers'),
                   ContainerStatus('Docker monitor', 'dockerstatus', 'Flask webserver, providing this webpage'),
-                  ContainerStatus('Who am I?', 'whoami', 'Simple container that exposes its container id via http')]
-    data = getContainerData(DATA_DIR)
+                  ContainerStatus('Who am I?', 'whoami', 'Simple container that exposes its container id via http'),
+                  ContainerStatus("Let's encrypt", 'letsencrypt', 'Companion container for the proxy that automatically installs ssl certificates for other containers')]
+    data, time = getContainerData(DATA_DIR)
     for c in containers:
         c.update_status(data)
-    return flask.render_template('dockerstatus.html', containers=containers)
+    return flask.render_template('dockerstatus.html', containers=containers, ps_timestamp=str(time))
 
 def getContainerData(data_dir):
     data_rows = []
+    time = None
     with open(os.path.join(data_dir, 'ps.txt')) as f:
         for i,l in enumerate(f):
-            data = l.split(';')
-            data = [d.strip() for d in data]
-            data_rows.append(dict(id=data[0], name=data[1], status=data[2]))
-    return data_rows
+            if i == 0:
+                time = datetime.fromisoformat(l.strip())
+            else:
+                data = l.split(';')
+                data = [d.strip() for d in data]
+                data_rows.append(dict(id=data[0], name=data[1], status=data[2]))
+    return data_rows, time
 
 if __name__ == '__main__':
     # data = getContainerData('data')
