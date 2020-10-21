d708e4b031312fa8656f6a699b2d924930aa8f35
Michael Scherbela <michael.scherbela@gmail.com>
2020-10-21 20:30:07 +0200

    Switched to using docker SDK

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import flask
 import os
 from datetime import datetime
+import docker
 
 app = flask.Flask(__name__)
-DATA_DIR = '/data'
+DOCKER_SOCKET = 'unix://var/run/docker.sock'
 
 class ContainerStatus:
     def __init__(self, title, container_name, description=""):
@@ -13,40 +14,33 @@ class ContainerStatus:
         self.status_text = ""
         self.up = False
 
-    def update_status(self, container_data):
-        for c in container_data:
-            if (c['name'] == self.container_name) and ("up" in c['status'].lower()):
-                self.status_text = c['status']
-                self.up = True
-                break
-        else:
-            self.status_text = ""
-            self.up = False
+    def update_status(self, running_containers):
+        self.up = False
+        self.status_text = ""
+        for c in running_containers:
+            if (c.name == self.container_name):
+                self.status_text = str(c.status)
+                if 'up' in self.status_text.lower():
+                    self.up = True
+                    break
 
 @app.route('/')
-def hello_world():
-    containers = [ContainerStatus('Paragliding Weather Scraping', 'paragliding', 'Simple webscraper that logs hourly weather condition and forecasts + the status of flying/schooling conditions at Hohe Wand.'),
-                  ContainerStatus('Main webserver', 'proxy', 'Main webserver that acts as entry point before dispatching to other sub-servers'),
-                  ContainerStatus('Docker monitor', 'dockerstatus', 'Flask webserver, providing this webpage'),
-                  ContainerStatus('Who am I?', 'whoami', 'Simple container that exposes its container id via http'),
-                  ContainerStatus("Let's encrypt", 'letsencrypt', 'Companion container for the proxy that automatically installs ssl certificates for other containers')]
-    data, time = getContainerData(DATA_DIR)
-    for c in containers:
-        c.update_status(data)
-    return flask.render_template('dockerstatus.html', containers=containers, ps_timestamp=str(time))
+def status():
+    target_containers = [ContainerStatus('Paragliding Weather Scraping', 'paragliding', 'Simple webscraper that logs hourly weather condition and forecasts + the status of flying/schooling conditions at Hohe Wand.'),
+                         ContainerStatus('Main webserver', 'proxy', 'Main webserver that acts as entry point before dispatching to other sub-servers'),
+                         ContainerStatus('Docker monitor', 'dockerstatus', 'Flask webserver, providing this webpage'),
+                         ContainerStatus('Who am I?', 'whoami', 'Simple container that exposes its container id via http'),
+                         ContainerStatus("Let's encrypt", 'letsencrypt', 'Companion container for the proxy that automatically installs ssl certificates for other containers')]
+    running_containers = getRunningContainers()
+    for c in target_containers:
+        c.update_status(running_containers)
+    return flask.render_template('dockerstatus.html', containers=target_containers)
 
-def getContainerData(data_dir):
-    data_rows = []
-    time = None
-    with open(os.path.join(data_dir, 'ps.txt')) as f:
-        for i,l in enumerate(f):
-            if i == 0:
-                time = datetime.fromisoformat(l.strip())
-            else:
-                data = l.split(';')
-                data = [d.strip() for d in data]
-                data_rows.append(dict(id=data[0], name=data[1], status=data[2]))
-    return data_rows, time
+def getRunningContainers():
+    """Returns a list of the names of running docker containers"""
+    client = docker.DockerClient(DOCKER_SOCKET)
+    containers = client.containers.list()
+    return containers
 
 if __name__ == '__main__':
     # data = getContainerData('data')
